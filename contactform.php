7a3f4e14922d8f9afa827a3afc112bb0014be91d
farukozhan <65257604+farukozhan@users.noreply.github.com>
2022-08-03 00:21:21 -0400

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,18 @@
+<?<php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['Your Name'];
+    $subject = $_POST['Subject'];
+    $mailFrom = $_POST['Your Email'];
+    $name = $_POST['Your Message'];
+    $mailTo = "info@theimperialtrade.com";
+    $headers = "From: F and O square".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.html?mailsend");
+
+
+}
+
+></php>
