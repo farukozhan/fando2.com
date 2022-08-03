8aaac3da71abf144198893aba4ce5206316333bd
farukozhan <65257604+farukozhan@users.noreply.github.com>
2022-08-03 00:44:29 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Contact With Us </title>
+    <link rel="shortcut icon" type="image/png" href="C:\Users\User\OneDrive\Desktop\website\images\favicon.png">
+    <link rel="stylesheet" href="style.css"
+</head>
+<body>
+    <section class="sec">
+        <header>
+            <nav class="navbar">
+
+                <a href="#" class="nav-branding"><img src="C:\Users\User\OneDrive\Desktop\website\images\logocircle.png" class="logo"></a>
+                <ul class="nav-menu">
+                    <li class="nav-item"><a href="C:\Users\User\OneDrive\Desktop\website\index.html" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="C:\Users\User\OneDrive\Desktop\website\products.html" class="nav-link">Products</a></li>
+                    <li class="nav-item"><a href="C:\Users\User\OneDrive\Desktop\website\contact.html" class="nav-link">Contact</a></li>
+                </ul>
+                <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+                </div>
+            </nav>
+        </header>
+        <script src="C:\Users\User\OneDrive\Desktop\website\script.js"></script>
+        <ul class="sci">
+            <li><a href="#"><img src="C:\Users\User\OneDrive\Desktop\website\images\facebook.png"</a></li>
+            <li><a href="#"><img src="C:\Users\User\OneDrive\Desktop\website\images\twitter.png"</a></li>
+            <li><a href="#"><img src="C:\Users\User\OneDrive\Desktop\website\images\instagram.png"</a></li>
+        </ul>
+        <div class="contact-box">
+            <form class="contact-form" action="contactform.php" method="post">
+                <input type="text" class="input-field" placeholder="Your Name" >
+                <input type="text" class="input-field" placeholder="Your Email" >
+                <input type="text" class="input-field" placeholder="Subject" >
+                <textarea type="text" class="input-field textarea-field" placeholder="Your Message">
+                </textarea>
+                <button type="submit" name="submit" class="btn">Send Message</button>
+
+            </form>
+        </div>
+    
+    </section>
+</body>
+</html>
