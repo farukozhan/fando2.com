c5163d79f22e19b908bd09426090eeb6b2d2cd23
Mustafa Bayram <byrmstf@yandex.com>
2024-03-15 09:12:12 +0300

    Çalışır haline geri getirildi.

--- contactMail.php
+++ contactMail.php
@@ -9,25 +9,26 @@ require 'phpmailler/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
-if(isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
     $mail = new PHPMailer(true);
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
+    $mail->Host = 'smtp.yandex.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'byrmstfg@gmail.com';
-    $mail->Password = 'drkgyxipkuyjzmvj';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
+    $mail->Username = 'info@theimperialtrade.com';
+    $mail->Password = 'B@b@t@k@r44';
+    $mail->SMTPSecure = 'SSL/TLS';
+    $mail->Port = 465;
 }
 $body = "<h3>İletişim Formu</h3> <br><b>İsim:</b> "
-. $_POST['fullname']."<br><b>E-posta:</b> "
-. $_POST['mail']. "<br><b>Mesaj:</b> ".$_POST["message"] ; 
+    . $_POST['fullname']."<br><b>E-posta:</b> "
+    . $_POST['mail']. "<br><b>Mesaj:</b> ".$_POST["message"] ;
 
-$mail->setFrom('byrmstfg@gmail.com'); // Kendi Gmail adresiniz
-$mail->addAddress("byrmstfg@gmail.com");
+$mail->setFrom('info@theimperialtrade.com'); // Your Company Name
+$mail->addAddress("ozhanfaruk@gmail.com");
 $mail->isHTML(true);
 $mail->Subject = $_POST["subject"];
 $mail->Body = $body;
+
 
 if ($mail->send()){
     echo
